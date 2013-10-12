374cbf46cc57b524184372383021b72bb16c6ab4
Nathan Nutter <iam@nnutter.com>
2013-10-11 20:12:51 -0500

    rename realrealpath to realpath
    Change-Id: I06c64317b2c8a66800f9346ff7f43f703823422c

--- t/lib/Test/GitGerrit.pm
+++ t/lib/Test/GitGerrit.pm
@@ -6,12 +6,12 @@ use base 'Test::Builder::Module';
 use strict;
 use warnings;
 
-use Cwd qw(realpath);
+use Cwd qw();
 use File::Basename qw(dirname);
 use File::Spec qw();
 
 my $parent_dir = dirname(__FILE__);
-my $git_gerrit_dir = realrealpath($parent_dir, '..', '..', '..');
+my $git_gerrit_dir = realpath($parent_dir, '..', '..', '..');
 
 my $bin_path = File::Spec->join($git_gerrit_dir, 'git-gerrit');
 unless (-e $bin_path) {
@@ -23,8 +23,8 @@ unless (-x $bin_path) {
 
 $ENV{PATH} = "$git_gerrit_dir:$ENV{PATH}";
 
-sub realrealpath {
-    return realpath(File::Spec->join(@_));
+sub realpath {
+    return Cwd::realpath(File::Spec->join(@_));
 }
 
 1;
