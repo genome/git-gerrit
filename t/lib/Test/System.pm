d4a30cfffe293b48c7e0c09d83caff02a5cb94be
Nathan Nutter <iam@nnutter.com>
2013-10-11 20:56:15 -0500

    add exit_is test function
    Change-Id: I027b393ef014dd0a1ad27fde09141ac8a825ff84

--- t/lib/Test/System.pm
+++ t/lib/Test/System.pm
@@ -5,6 +5,7 @@ package Test::System;
 use base 'Test::Builder::Module';
 our @EXPORT_OK = qw(
     run_ok
+    exit_is
 );
 
 use Test::More import => [qw(diag)];
@@ -12,9 +13,18 @@ use Test::More import => [qw(diag)];
 use IPC::System::Simple qw(capture);
 
 sub run_ok {
-    my ($command, $test_name) = @_;
+    return _exit_is(0, @_);
+}
+
+sub exit_is {
+    return _exit_is(@_);
+}
+
+sub _exit_is {
+    my ($expected_exit, $command, $test_name) = @_;
 
     my $tb = __PACKAGE__->builder;
+    $tb->level(2);
 
     my @command = ref $command ? @$command : $command;
     $test_name //= @command > 1 ? join(' ', @command) : $command[0];
@@ -22,7 +32,8 @@ sub run_ok {
     # using capture because I like not having to escape arguments and I would
     # like to only show output on failure
     my $output = eval { capture(@command) } || 'NO OUTPUT';
+    my $exit = $? >> 8;
 
-    $tb->ok($? == 0, $test_name)
-        or diag sprintf("exit code: %d\noutput: %s\n", $? >> 8, $output);
+    $tb->ok($exit == $expected_exit, $test_name)
+        or diag sprintf("exit code: %d\noutput: %s\n", $exit, $output);
 }
