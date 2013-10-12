5ad5d51f3e3fec2c34a0c45c50aebb8c2782c613
Nathan Nutter <iam@nnutter.com>
2013-10-11 20:13:43 -0500

    rename git_gerrit_dir to top_dir
    Change-Id: I6f8c63cf7b89e518f82865dc498e94249cfc3de1

--- t/lib/Test/GitGerrit.pm
+++ t/lib/Test/GitGerrit.pm
@@ -11,17 +11,17 @@ use File::Basename qw(dirname);
 use File::Spec qw();
 
 my $parent_dir = dirname(__FILE__);
-my $git_gerrit_dir = realpath($parent_dir, '..', '..', '..');
+my $top_dir = realpath($parent_dir, '..', '..', '..');
 
-my $bin_path = File::Spec->join($git_gerrit_dir, 'git-gerrit');
+my $bin_path = File::Spec->join($top_dir, 'git-gerrit');
 unless (-e $bin_path) {
-    die "could not find git-gerrit in directory: $git_gerrit_dir";
+    die "could not find git-gerrit in directory: $top_dir";
 }
 unless (-x $bin_path) {
     die "git-gerrit is not executable: $bin_path";
 }
 
-$ENV{PATH} = "$git_gerrit_dir:$ENV{PATH}";
+$ENV{PATH} = "$top_dir:$ENV{PATH}";
 
 sub realpath {
     return Cwd::realpath(File::Spec->join(@_));
